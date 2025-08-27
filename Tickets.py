8675dcfdff15be18523916831332b7e93ab693ff
Brenda <brentorres26@gmail.com>
2025-08-27 17:22:42 -0300

    Configuración del dashboard, título, lectura de archivo

--- Tickets.py
+++ Tickets.py
@@ -1,3 +1,16 @@
 import pandas as od
 import streamlit as st
 import matplotlib.pyplot as plt
+
+#------------------------------Dashboard
+st.set_page_config(
+    page_title="Tickets",
+    page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title("📈 Optimización del rendimiento del soporte de TI")
+
+# Cargamos el dataset
+df = pd.read_excel("C:/Users/brent/Downloads/IT_Support_Ticket_Spanish.xlsx")
